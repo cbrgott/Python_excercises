1684187a91d88c0c790e7589af7db8e8b78b42e1
cbrgott <52463232+cbrgott@users.noreply.github.com>
2022-10-25 17:39:00 -0500

    Update python_excercises.py

--- python_excercises.py
+++ python_excercises.py
@@ -40,7 +40,7 @@ class Solution:
             median = listx[int((z-1)/2)]
         return median
 
-####### GCA ########
+####### CODE SIGNAL ########
 # You are given an array of integers a and two integers l and r. You task is to calculate a boolean array b, where b[i] = true if there exists an integer x, such that a[i] = (i + 1) * x and l ≤ x ≤ r. Otherwise, b[i] should be set to false.
 
 # Example
